76ba1273b8c5f4d5df94304b02951d35644478da
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-04 11:58:11 +0000

    fizzBuzz challenge solved. Some help from the solution and some help from the tutor. There was a typo in the HTML that caused most of the issues

--- jasmine-challenge-fizzBuzzGame/spec/fizzbuzzSpec.js
+++ jasmine-challenge-fizzBuzzGame/spec/fizzbuzzSpec.js
@@ -3,8 +3,13 @@ describe("My fizzBuzz function", function() {
     beforeEach(function() {
         fizzbuzz = new fizzBuzz();
     });
+    
 
-    describe("Returns one of four result words", function() {
+    describe("Returns one of four possible results", function() {
+        
+        it("should exist", function(){
+            expect(fizzBuzz).toBeDefined();
+        });
 
         it("should return FizzBuzz", function() {
             expect(fizzBuzz(30)).toBe("FizzBuzz");
@@ -16,10 +21,13 @@ describe("My fizzBuzz function", function() {
             expect(fizzBuzz(25)).toBe("Buzz");
         });
         it("should return Number", function() {
-            expect(fizzBuzz(44)).toBe(number);
+            expect(fizzBuzz(44)).toBe(44);
         });
         it("should return Number", function() {
-            expect(fizzBuzz("perro")).toBe(number);
+            expect(fizzBuzz(19)).toBe(19);
+        });
+        it("should return Number", function() {
+            expect(fizzBuzz("dog")).toBe("dog");
         });
     })
 })
